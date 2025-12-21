5bf9a5e0bbf69d55d05fee5aa358ac6e9f4f7867
Tamir Rustamov <timorust1@gmail.com>
2025-12-21 08:30:03 +0200

    bubbleSort

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+function bubbleSort(arr) {
+  const n = arr.length;
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+
+    console.log(arr);
+  }
+  return arr;
+}
+
+bubbleSort([11, 2, 1, 4, 3, 8, 5]);
